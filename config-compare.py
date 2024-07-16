5291cd6e0aee65f792f727435b941c7ff6eec3fc
jingchunhe888 <132230624+jingchunhe888@users.noreply.github.com>
2024-07-16 00:05:16 -0400

    Update and rename all.py to config-compare.py

---
+++ config-compare.py
@@ -0,0 +1,23 @@
+#File has been edited to folllow NDA Agreement
+# QB File
+QBFile = r""
+QBskipRow = 4
+QBColumns = ['Date', 'Name', 'Amount', 'Clr']
+
+# Bank Statement
+ActivityFile = r""
+ActivityColumns = ['Posting Date', 'Trans. Date', 'CardHolder Name', 'Amount', 'MCC']
+ActivitySkipRow = 4
+ActivitySkipColumn = 0
+
+# Vendor File
+VendorFile = r''
+VendorSheet = 'Sheet1'
+VendorColumn = ['Vendor']
+
+PriceColumn = 'Amount'
+FuzzyMatch = 70
+Font = "Sans Serif Collection"
+HeadingSize = 18
+NormalSize = 12
+MissingFile = r'/Users/jinhe/Downloads/goldfine/Missing Transactions.xlsx'
